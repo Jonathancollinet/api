5f300d3762e323735c92ef552f10cd475a72a257
Jonathancollinet <jonathan.collinet@epitech.eu>
2015-01-23 17:13:41 +0100

    add crud

---
+++ api/users/index.js
@@ -0,0 +1,77 @@
+exports.listAll = function(req, res) {
+	req.app.db.models.User.find().exec(function(err, row) {
+		if (!err)
+			res.json({data: row});
+		else
+			res.json({data: err});
+	});
+}
+
+exports.findOne = function(req, res) {
+	req.app.db.models.User.findOne().exec(function(err, row) {
+		if (!err)
+			res.json({data: row});
+		else
+			res.json({data: err});
+	});
+}
+
+exports.count = function(req, res) {
+	req.app.db.models.User.find().count().exec(function(err, row) {
+		if (!err)
+			res.json({data: row});
+		else
+			res.json({data: err});
+	});
+}
+
+exports.findId = function(req, res) {
+	req.app.db.models.User.findById(req.params.id).exec(function(err, row) {
+		if (!err)
+			res.json({data: row});
+		else
+			res.json({data: err});
+	});
+}
+
+exports.exists = function(req, res) {
+	req.app.db.models.User.findById(req.params.id).exec(function(err, row) {
+		if (!err)
+			res.json({data: row});
+		else
+			res.json({data: err});
+	});
+}
+
+exports.create = function(req, res) {
+	req.app.db.models.User.create(req.body, function(err, row) {
+		if (!err)
+			res.json({data: row});
+		else
+			res.json({data: err});
+	});
+}
+
+exports.updateId = function(req, res) {
+	req.app.db.models.User.find().exec(function(err, row) {
+		if (!err)
+			res.json({data: row});
+		else
+			res.json({data: err});
+	});
+}
+
+exports.delete = function(req, res) {
+	req.app.db.models.User.findById(req.params.id).exec(function(err, row) {
+		if (!err && row) {
+			row.remove(function(err){
+				if (err)
+					res.json({data: err});
+				else
+					res.json({data: "Deleted"});
+			});
+		}
+		else
+			res.json({data: err || "Cannot find id : " + req.params.id});
+	});
+}
--- routes.js
+++ routes.js
@@ -4,4 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', require('./views/homepage').init);
 
+// users
+router.get('/users', require('./api/users/index').listAll);
+router.get('/users/findOne', require('./api/users/index').findOne);
+router.get('/users/count', require('./api/users/index').count);
+router.get('/users/:id', require('./api/users/index').findId);
+router.get('/users/:id/exists', require('./api/users/index').exists);
+router.post('/users', require('./api/users/index').create);
+router.put('/users/:id', require('./api/users/index').updateId);
+router.delete('/users/:id', require('./api/users/index').delete);
+
 module.exports = router;
