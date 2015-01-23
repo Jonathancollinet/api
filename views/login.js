63bbc66d51dcd72f4133711dd27328cd2f814154
healiha <healiha24@gmail.com>
2015-01-23 17:30:28 +0100

    Reset login

--- views/login.js
+++ views/login.js
@@ -1,9 +1,9 @@
 var request = require('request'),
     acceptedAuth = {
       'facebook': 'https://graph.facebook.com/me?access_token=',
-      'google': 'https://www.googleapis.com/oauth2/v1/userinfo?token_type=Bearer&access_token='
+      'google': 'https://www.googleapis.com/oauth2/v1/userinfo'
     };
 
 exports.init = function(req, res) {
-  return res.json(req.body);
+  return res.json(JSON.parse(req.body));
 };
