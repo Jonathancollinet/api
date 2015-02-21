db59b85bc69d802a05e42068665dc61ae3a7dca1
Jonathancollinet <jonathan.collinet@epitech.eu>
2015-02-21 18:11:17 +0100

    add parenthesis on regex

--- api/search/index.js
+++ api/search/index.js
@@ -1,5 +1,5 @@
 exports.users = function(req, res, next) {
-	req.app.db.models.User.findOne({username: new RegExp(req.body.query+'*', "i")}).exec(function(err, row) {
+	req.app.db.models.User.findOne({username: new RegExp("("+req.body.query+")", "i")}).exec(function(err, row) {
 		if (err)
 			return next(err);
 		res.json({data: row});
